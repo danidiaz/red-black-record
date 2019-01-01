2d2b77ce696b5a8820a2a8ba6ecea99b0df647ce
Daniel Diaz <diaz.carrete@facebook.com>
2019-01-01 23:25:59 +0100

    next: add balanceV and widen

--- lib/Data/RBR/Internal.hs
+++ lib/Data/RBR/Internal.hs
@@ -43,28 +43,34 @@ data Variant (f :: Type -> Type) (kv :: RBT Symbol Type)  where
 class Insertable (k :: Symbol) (v :: Type) (t :: RBT Symbol Type) where
     type Insert k v t :: RBT Symbol Type
     insert :: f v -> Record f t -> Record f (Insert k v t)
+    widen :: Variant f t -> Variant f (Insert k v t)
 
 instance (InsertableHelper1 k v t, CanMakeBlack (Insert1 k v t)) => Insertable k v t where
     type Insert k v t = MakeBlack (Insert1 k v t)
     insert fv r = makeBlackR (insert1 @k @v fv r) 
+    widen v = makeBlackV (widen1 @k @v v)
 
 class CanMakeBlack (t :: RBT Symbol Type) where
     type MakeBlack t :: RBT Symbol Type
     makeBlackR :: Record f t -> Record f (MakeBlack t)
+    makeBlackV :: Variant f t -> Variant f (MakeBlack t)
 
 instance CanMakeBlack (N color left k v right) where
     type MakeBlack (N color left k v right) = N B left k v right
     makeBlackR (Node left fv right) = Node left fv right
+    makeBlackV (Here fv) = Here fv
 
 class InsertableHelper1 (k :: Symbol) 
                         (v :: Type) 
                         (t :: RBT Symbol Type) where
     type Insert1 k v t :: RBT Symbol Type 
     insert1 :: f v -> Record f t -> Record f (Insert1 k v t)
+    widen1 :: Variant f t -> Variant f (Insert1 k v t)
 
 instance InsertableHelper1 k v E where
     type Insert1 k v E = N R E k v E
     insert1 fv Empty = Node Empty fv Empty 
+    widen1 v = undefined
 
 instance (CmpSymbol k k' ~ ordering, 
           InsertableHelper2 ordering k v color left k' v' right
@@ -74,6 +80,7 @@ instance (CmpSymbol k k' ~ ordering,
     -- Is that bad? How to avoid it?
     type Insert1 k v (N color left k' v' right) = Insert2 (CmpSymbol k k') k v color left k' v' right  
     insert1 = insert2 @ordering @k @v @color @left @k' @v' @right
+    widen1 v = undefined
 
 class InsertableHelper2 (ordering :: Ordering) 
                         (k :: Symbol) 
@@ -85,24 +92,28 @@ class InsertableHelper2 (ordering :: Ordering)
                         (right :: RBT Symbol Type) where
     type Insert2 ordering k v color left k' v' right :: RBT Symbol Type 
     insert2 :: f v -> Record f (N color left k' v' right) -> Record f (Insert2 ordering k v color left k' v' right)
+    widen2 :: Variant f (N color left k' v' right) -> Variant f (Insert2 ordering k v color left k' v' right)
 
 instance (InsertableHelper1 k v left,
           Balanceable color (Insert1 k v left) k' v' right
          )
          => InsertableHelper2 LT k v color left k' v' right where
     type Insert2 LT k v color left k' v' right = Balance color (Insert1 k v left) k' v' right
-    insert2 fv (Node left fv' right) = balance @color @_ @k' @v' @right (Node (insert1 @k @v fv left) fv' right) 
+    insert2 fv (Node left fv' right) = balanceR @color @_ @k' @v' @right (Node (insert1 @k @v fv left) fv' right) 
+    widen2 v = undefined
 
 instance InsertableHelper2 EQ k v color left k' v' right where
     type Insert2 EQ k v color left k' v' right = N color left k v right
     insert2 fv (Node left _ right) = Node left fv right
+    widen2 v = undefined
 
 instance (InsertableHelper1 k v right,
           Balanceable color left  k' v' (Insert1 k v right)
          )
          => InsertableHelper2 GT k v color left k' v' right where
     type Insert2 GT k v color left k' v' right = Balance color left  k' v' (Insert1 k v right)
-    insert2 fv (Node left fv' right) = balance @color @left @k' @v' @_ (Node left  fv' (insert1 @k @v fv right)) 
+    insert2 fv (Node left fv' right) = balanceR @color @left @k' @v' @_ (Node left  fv' (insert1 @k @v fv right)) 
+    widen2 v = undefined
 
 data BalanceAction = BalanceLL
                    | BalanceLR
@@ -126,7 +137,7 @@ class Balanceable (color :: Color)
                   (v :: Type) 
                   (right :: RBT Symbol Type) where
     type Balance color left k v right :: RBT Symbol Type
-    balance :: Record f (N color left k v right) -> Record f (Balance color left k v right)
+    balanceR :: Record f (N color left k v right) -> Record f (Balance color left k v right)
 
 instance (ShouldBalance color left right ~ action, 
           BalanceableHelper action color left k v right
@@ -135,7 +146,7 @@ instance (ShouldBalance color left right ~ action,
     -- FIXME possible duplicate work with ShouldBalance: both in constraint and in associated type family. 
     -- Is that bad? How to avoid it?
     type Balance color left k v right = Balance' (ShouldBalance color left right) color left k v right
-    balance = balance' @action @color @left @k @v @right
+    balanceR = balanceR' @action @color @left @k @v @right
     
 class BalanceableHelper (action :: BalanceAction) 
                         (color :: Color) 
@@ -144,27 +155,27 @@ class BalanceableHelper (action :: BalanceAction)
                         (v :: Type) 
                         (right :: RBT Symbol Type) where
     type Balance' action color left k v right :: RBT Symbol Type
-    balance' :: Record f (N color left k v right) -> Record f (Balance' action color left k v right)
+    balanceR' :: Record f (N color left k v right) -> Record f (Balance' action color left k v right)
 
 instance BalanceableHelper BalanceLL B (N R (N R a k1 v1 b) k2 v2 c) k3 v3 d where
     type Balance' BalanceLL B (N R (N R a k1 v1 b) k2 v2 c) k3 v3 d = N R (N B a k1 v1 b) k2 v2 (N B c k3 v3 d)
-    balance' (Node (Node (Node a fv1 b) fv2 c) fv3 d) = Node (Node a fv1 b) fv2 (Node c fv3 d)
+    balanceR' (Node (Node (Node a fv1 b) fv2 c) fv3 d) = Node (Node a fv1 b) fv2 (Node c fv3 d)
 
 instance BalanceableHelper BalanceLR B (N R a k1 v1 (N R b k2 v2 c)) k3 v3 d where
     type Balance' BalanceLR B (N R a k1 v1 (N R b k2 v2 c)) k3 v3 d = N R (N B a k1 v1 b) k2 v2 (N B c k3 v3 d) 
-    balance' (Node (Node a fv1 (Node b fv2 c)) fv3 d) = Node (Node a fv1 b) fv2 (Node c fv3 d)
+    balanceR' (Node (Node a fv1 (Node b fv2 c)) fv3 d) = Node (Node a fv1 b) fv2 (Node c fv3 d)
 
 instance BalanceableHelper BalanceRL B a k1 v1 (N R (N R b k2 v2 c) k3 v3 d) where
     type Balance' BalanceRL B a k1 v1 (N R (N R b k2 v2 c) k3 v3 d) = N R (N B a k1 v1 b) k2 v2 (N B c k3 v3 d) 
-    balance' (Node a fv1 (Node (Node b fv2 c) fv3 d)) = Node (Node a fv1 b) fv2 (Node c fv3 d)
+    balanceR' (Node a fv1 (Node (Node b fv2 c) fv3 d)) = Node (Node a fv1 b) fv2 (Node c fv3 d)
 
 instance BalanceableHelper BalanceRR B a k1 v1 (N R b k2 v2 (N R c k3 v3 d)) where
     type Balance' BalanceRR B a k1 v1 (N R b k2 v2 (N R c k3 v3 d)) = N R (N B a k1 v1 b) k2 v2 (N B c k3 v3 d) 
-    balance' (Node a fv1 (Node b fv2 (Node c fv3 d))) = Node (Node a fv1 b) fv2 (Node c fv3 d)
+    balanceR' (Node a fv1 (Node b fv2 (Node c fv3 d))) = Node (Node a fv1 b) fv2 (Node c fv3 d)
 
 instance BalanceableHelper DoNotBalance color a k v b where
     type Balance' DoNotBalance color a k v b = N color a k v b 
-    balance' = id
+    balanceR' = id
 
 --
 --
