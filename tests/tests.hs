0bcea84d24a03b94e05d873532b874428bebf6f6
Daniel Diaz <diaz.carrete@facebook.com>
2019-01-13 14:28:07 +0100

    first test

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE DataKinds,
+             TypeApplications,
+             PartialTypeSignatures
+#-}
+{-#  OPTIONS_GHC -Wno-partial-type-signatures  #-}
 module Main where
+
+import Data.RBR
+import Data.SOP
 
 import Test.Tasty
 import Test.Tasty.HUnit (testCase,assertEqual,assertBool)
@@ -7,5 +15,19 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [
+tests = testGroup "Tests" [ testCase "testProjectSubset01" testProjectSubset01
                           ]
+
+testProjectSubset01 :: IO ()
+testProjectSubset01  = do
+    let r = insertI @"foo" 'c'
+          . insertI @"bar" True
+          . insertI @"baz" (1::Int)
+          $ unit
+        s = projectSubset @(FromList '[ '("bar",_),
+                                        '("baz",_) ]) r
+        bar = getFieldI @"bar" s
+        baz = getFieldI @"baz" s
+    assertEqual "bar" bar True
+    assertEqual "baz" baz 1
+
