eb0a054012795efcbb6d358b91a2ab6f3e6ebc4e
Daniel Diaz <diaz.carrete@facebook.com>
2019-02-17 18:58:05 +0100

    patched bug in deletion code

--- lib/Data/RBR/Internal.hs
+++ lib/Data/RBR/Internal.hs
@@ -1414,13 +1414,17 @@ class DelableL (k :: Symbol) (v :: Type) (t :: RBT Symbol Type) where
     type DelL k v t :: RBT Symbol Type
     delL :: Record f t -> Record f (DelL k v t)
 
-instance (Delable k v left, BalanceableL (N B (Del k v left) kx vx right)) => DelableL k v (N B left kx vx right) where
-    type DelL k v (N B left kx vx right) = BalL (N B (Del k v left) kx vx right)
-    delL (Node left vx right) = balLR @(N B (Del k v left) kx vx right) (Node (del @k @v left) vx right)
+instance (Delable k v (N B leftz kz vz rightz), BalanceableL (N B (Del k v (N B leftz kz vz rightz)) kx vx right)) => DelableL k v (N color (N B leftz kz vz rightz) kx vx right) where
+    type DelL k v (N color (N B leftz kz vz rightz) kx vx right) = BalL (N B (Del k v (N B leftz kz vz rightz)) kx vx right)
+    delL (Node left vx right) = balLR @(N B (Del k v (N B leftz kz vz rightz)) kx vx right) (Node (del @k @v left) vx right)
 
-instance (Delable k v left) => DelableL k v (N R left kx vx right) where
-    type DelL k v (N R left kx vx right) = N R (Del k v left) kx vx right
+instance (Delable k v (N R leftz kz vz rightz)) => DelableL k v (N R (N R leftz kz vz rightz) kx vx right) where
+    type DelL k v (N R (N R leftz kz vz rightz) kx vx right) = N R (Del k v (N R leftz kz vz rightz)) kx vx right
     delL (Node left vx right) = Node (del @k @v left) vx right
+
+instance DelableL k v E where
+    type DelL k v E = E
+    delL _ = unit
 
 -- delR :: (Ord a) => a -> Tree a -> Tree a
 -- delR x t@(T B t1 y t2) = balR $ T B t1 y (del x t2)
@@ -1430,19 +1434,27 @@ class DelableR (k :: Symbol) (v :: Type) (t :: RBT Symbol Type) where
     type DelR k v t :: RBT Symbol Type
     delR :: Record f t -> Record f (DelR k v t)
 
-instance (Delable k v right, BalanceableR (N B left kx vx (Del k v right))) => DelableR k v (N B left kx vx right) where
-    type DelR k v (N B left kx vx right) = BalR (N B left kx vx (Del k v right))
-    delR (Node left vx right) = balRR @(N B left kx vx (Del k v right)) (Node left vx (del @k @v right))
+instance (Delable k v (N B leftz kz vz rightz), BalanceableR (N B left kx vx (Del k v (N B leftz kz vz rightz)))) => DelableR k v (N color left kx vx (N B leftz kz vz rightz)) where
+    type DelR k v (N color left kx vx (N B leftz kz vz rightz)) = BalR (N B left kx vx (Del k v (N B leftz kz vz rightz)))
+    delR (Node left vx right) = balRR @(N B left kx vx (Del k v (N B leftz kz vz rightz))) (Node left vx (del @k @v right))
 
-instance (Delable k v right) => DelableR k v (N R left kx vx right) where
-    type DelR k v (N R left kx vx right) = N R left kx vx (Del k v right)
+instance (Delable k v (N R leftz kz vz rightz)) => DelableR k v (N color left kx vx (N R leftz kz vz rightz)) where
+    type DelR k v (N color left kx vx (N R leftz kz vz rightz)) = N R left kx vx (Del k v (N R leftz kz vz rightz))
     delR (Node left vx right) = Node left vx (del @k @v right)
+
+instance DelableR k v E where
+    type DelR k v E = E
+    delR _ = unit
 
 -- del :: (Ord a) => a -> Tree a -> Tree a
 -- del x t@(T _ l y r)
 --   | x < y = delL x t
 --   | x > y = delR x t
 --   | otherwise = fuse l r
+
+instance Delable k v E where
+    type Del k v E = E
+    del _ = unit
 
 instance (CmpSymbol kx k ~ ordering, DelableHelper ordering k v (N color left kx vx right)) => Delable k v (N color left kx vx right) where
     type Del k v (N color left kx vx right) = Del' (CmpSymbol kx k) k v (N color left kx vx right)
@@ -1491,3 +1503,87 @@ instance (Delable k v t, CanMakeBlack (Del k v t)) => Deletable k v t where
 --                                                                     Symbol *
 -- = BalR' 'True ('N 'B 'E "bar" Bool 'E)
 -- ╬ø--                                                                    Symbol *
+
+-- {- Version 1, 'untyped' -}
+-- data Color = R | B deriving Show
+-- data RB a = E | T Color (RB a) a (RB a) deriving Show
+-- 
+-- {- Insertion and membership test as by Okasaki -}
+-- insert :: Ord a => a -> RB a -> RB a
+-- insert x s =
+-- 	T B a z b
+-- 	where
+-- 	T _ a z b = ins s
+-- 	ins E = T R E x E
+-- 	ins s@(T B a y b)
+-- 		| x<y = balance (ins a) y b
+-- 		| x>y = balance a y (ins b)
+-- 		| otherwise = s
+-- 	ins s@(T R a y b)
+-- 		| x<y = T R (ins a) y b
+-- 		| x>y = T R a y (ins b)
+-- 		| otherwise = s
+-- 
+-- member :: Ord a => a -> RB a -> Bool
+-- member x E = False
+-- member x (T _ a y b)
+-- 	| x<y = member x a
+-- 	| x>y = member x b
+-- 	| otherwise = True
+-- 
+-- {- balance: first equation is new,
+--    to make it work with a weaker invariant -}
+-- balance :: RB a -> a -> RB a -> RB a
+-- balance (T R a x b) y (T R c z d) = T R (T B a x b) y (T B c z d)
+-- balance (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
+-- balance (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
+-- balance a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
+-- balance a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+-- balance a x b = T B a x b
+-- 
+-- {- deletion a la SMK -}
+-- delete :: Ord a => a -> RB a -> RB a
+-- delete x t =
+-- 	case del t of {T _ a y b -> T B a y b; _ -> E}
+-- 	where
+-- 	del E = E
+-- 	del (T _ a y b)
+-- 	    | x<y = delformLeft a y b
+-- 	    | x>y = delformRight a y b
+--             | otherwise = app a b
+-- 	delformLeft a@(T B _ _ _) y b = balleft (del a) y b
+-- 	delformLeft a y b = T R (del a) y b
+--
+-- 	delformRight a y b@(T B _ _ _) = balright a y (del b)
+-- 	delformRight a y b = T R a y (del b)
+-- 
+-- balleft :: RB a -> a -> RB a -> RB a
+-- balleft (T R a x b) y c = T R (T B a x b) y c
+-- balleft bl x (T B a y b) = balance bl x (T R a y b)
+-- balleft bl x (T R (T B a y b) z c) = T R (T B bl x a) y (balance b z (sub1 c))
+-- 
+-- balright :: RB a -> a -> RB a -> RB a
+-- balright a x (T R b y c) = T R a x (T B b y c)
+-- balright (T B a x b) y bl = balance (T R a x b) y bl
+-- balright (T R a x (T B b y c)) z bl = T R (balance (sub1 a) x b) y (T B c z bl)
+-- 
+-- sub1 :: RB a -> RB a
+-- sub1 (T B a x b) = T R a x b
+-- sub1 _ = error "invariance violation"
+-- 
+-- app :: RB a -> RB a -> RB a
+-- app E x = x
+-- app x E = x
+-- app (T R a x b) (T R c y d) =
+-- 	case app b c of
+-- 	    T R b' z c' -> T R(T R a x b') z (T R c' y d)
+-- 	    bc -> T R a x (T R bc y d)
+-- app (T B a x b) (T B c y d) = 
+-- 	case app b c of
+-- 	    T R b' z c' -> T R(T B a x b') z (T B c' y d)
+-- 	    bc -> balleft a x (T B bc y d)
+-- app a (T R b x c) = T R (app a b) x c
+-- app (T R a x b) c = T R a x (app b c)
+
+-- delete @"foo" @Bool (insertI @"foo" True (insertI @"bar" False unit))
+--
