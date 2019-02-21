7eb501da2d090e6a565c19117a350c18f6e3482b
Daniel Diaz <diaz.carrete@facebook.com>
2019-02-22 00:07:57 +0100

    Put implementation in internal library.

--- lib-internal/Data/RBR/Internal.hs
+++ lib-internal/Data/RBR/Internal.hs
@@ -43,12 +43,12 @@ import           Data.SOP.NS (collapse_NS,ap_NS,injections,Injection)
 -- | The color of a node.
 data Color = R
            | B
-    deriving Show
+    deriving (Show,Eq)
 
 -- | The Red-Black tree. It will be used, as a kind, to index the 'Record' and 'Variant' types.
 data RBT k v = E 
              | N Color (RBT k v) k v (RBT k v)
-    deriving Show
+    deriving (Show,Eq)
 
 --
 --
@@ -303,8 +303,8 @@ class InsertableHelper2 (ordering :: Ordering)
     insert2 :: f v -> Record f (N color left k' v' right) -> Record f (Insert2 ordering k v color left k' v' right)
     widen2 :: Variant f (N color left k' v' right) -> Variant f (Insert2 ordering k v color left k' v' right)
 
--- 	ins s@(T B a y b)
--- 		| x<y = balance (ins a) y b
+--  ins s@(T B a y b)
+--      | x<y = balance (ins a) y b
 instance (InsertableHelper1 k v left,
           Balanceable (Insert1 k v left) k' v' right -- TODO remove B here
          )
@@ -316,8 +316,8 @@ instance (InsertableHelper1 k v left,
         LookLeft x -> LookLeft (widen1 @k @v x)
         LookRight x -> LookRight x
 
--- 	ins s@(T B a y b)
--- 		| x<y = balance (ins a) y b
+--  ins s@(T B a y b)
+--      | x<y = balance (ins a) y b
 instance (InsertableHelper1 k v left,
           Balanceable (Insert1 k v left) k' v' right-- TODO remove B here
          )
@@ -338,9 +338,9 @@ instance InsertableHelper2 EQ k v color left k v right where
     insert2 fv (Node left _ right) = Node left fv right
     widen2 = id
 
--- 	ins s@(T B a y b)
--- 		| ...
--- 		| x>y = balance a y (ins b)
+--  ins s@(T B a y b)
+--      | ...
+--      | x>y = balance a y (ins b)
 instance (InsertableHelper1 k v right,
           Balanceable left  k' v' (Insert1 k v right)
          )
@@ -352,9 +352,9 @@ instance (InsertableHelper1 k v right,
         LookLeft x -> LookLeft x
         LookRight x -> LookRight (widen1 @k @v x)
 
--- 	ins s@(T R a y b)
--- 		| ...
--- 		| x>y = T R a y (ins b)
+--  ins s@(T R a y b)
+--      | ...
+--      | x>y = T R a y (ins b)
 instance (InsertableHelper1 k v right,
           Balanceable left  k' v' (Insert1 k v right)
          )
@@ -1208,13 +1208,13 @@ instance (BalanceableHelper    (ShouldBalance (N R t2 u uv t3) l) (N R t2 u uv t
 -- app E x = x
 -- app x E = x
 -- app (T R a x b) (T R c y d) =
--- 	case app b c of
--- 	    T R b' z c' -> T R(T R a x b') z (T R c' y d)
--- 	    bc -> T R a x (T R bc y d)
+--  case app b c of
+--      T R b' z c' -> T R(T R a x b') z (T R c' y d)
+--      bc -> T R a x (T R bc y d)
 -- app (T B a x b) (T B c y d) = 
--- 	case app b c of
--- 	    T R b' z c' -> T R(T B a x b') z (T B c' y d)
--- 	    bc -> balleft a x (T B bc y d)
+--  case app b c of
+--      T R b' z c' -> T R(T B a x b') z (T B c' y d)
+--      bc -> balleft a x (T B bc y d)
 -- app a (T R b x c) = T R (app a b) x c
 -- app (T R a x b) c = T R a x (app b c)
 
@@ -1416,19 +1416,19 @@ instance (BalanceableL left1 k1 v1 (N B E k2 v2 right2)) => FuseableHelper2 E (N
                             LookRight right2 -> LookRight (LookRight right2))
 
 
--- 	del E = E
--- 	del (T _ a y b)
--- 	    | x<y = delformLeft a y b
--- 	    | x>y = delformRight a y b
+--  del E = E
+--  del (T _ a y b)
+--      | x<y = delformLeft a y b
+--      | x>y = delformRight a y b
 --             | otherwise = app a b
 class Delable (k :: Symbol) (v :: Type) (t :: RBT Symbol Type) where
     type Del k v t :: RBT Symbol Type
     del :: Record f t -> Record f (Del k v t)
     win :: Variant f t -> Either (Variant f (Del k v t)) (f v) 
 
--- 	delformLeft a@(T B _ _ _) y b = balleft (del a) y b
--- 	delformLeft a y b = T R (del a) y b
--- 	In the term-level code, the k to delete is already on the environment.
+--  delformLeft a@(T B _ _ _) y b = balleft (del a) y b
+--  delformLeft a y b = T R (del a) y b
+--  In the term-level code, the k to delete is already on the environment.
 class DelableL (k :: Symbol) (v :: Type) (l :: RBT Symbol Type) (kx :: Symbol) (vx :: Type) (r :: RBT Symbol Type) where
     type DelL k v l kx vx r :: RBT Symbol Type
     delL :: Record f (N color l kx vx r) -> Record f (DelL k v l kx vx r)
@@ -1458,8 +1458,8 @@ instance DelableL k v E kx vx right where
         Here vx -> Left (Here vx)
         LookRight r -> Left (LookRight r)
 
--- 	delformRight a y b@(T B _ _ _) = balright a y (del b)
--- 	delformRight a y b = T R a y (del b)
+--  delformRight a y b@(T B _ _ _) = balright a y (del b)
+--  delformRight a y b = T R a y (del b)
 class DelableR (k :: Symbol) (v :: Type) (l :: RBT Symbol Type) (kx :: Symbol) (vx :: Type) (r :: RBT Symbol Type) where
     type DelR k v l kx vx r :: RBT Symbol Type
     delR :: Record f (N color l kx vx r) -> Record f (DelR k v l kx vx r)
@@ -1494,7 +1494,7 @@ instance Delable k v E where
     win = impossible
 
 -- the color is discarded
--- 	del (T _ a y b)
+--  del (T _ a y b)
 instance (CmpSymbol kx k ~ ordering, DelableHelper ordering k v left kx vx right) => Delable k v (N color left kx vx right) where
     type Del k v (N color left kx vx right) = Del' (CmpSymbol kx k) k v left kx vx right
     del = del' @(CmpSymbol kx k) @k @v @left @kx @vx @right
@@ -1555,6 +1555,84 @@ instance (Delable k v t, CanMakeBlack (Del k v t)) => Deletable k v t where
 winnowI :: forall k v t . Deletable k v t => Variant I t -> Either (Variant I (Delete k v t)) v
 winnowI = fmap unI . winnow @k @v @t
 
+class DemotableColor (c :: Color) where
+    demoteColor :: Proxy c -> Color
+
+instance DemotableColor R where
+    demoteColor _ = R
+
+instance DemotableColor B where
+    demoteColor _ = B
+
+class DemotableMap (t :: RBT Symbol Type) where
+    demoteMap :: Proxy t -> RBT String TypeRep
+
+instance DemotableMap E where
+    demoteMap _ = E
+
+instance (DemotableColor c,
+          KnownSymbol s,
+          Typeable ty,
+          DemotableMap l,
+          DemotableMap r) 
+          => DemotableMap (N c l s ty r) where
+    demoteMap _ = N (demoteColor (Proxy @c)) 
+                    (demoteMap (Proxy @l)) 
+                    (symbolVal (Proxy @s)) 
+                    (typeRep (Proxy @ty)) 
+                    (demoteMap (Proxy @r))
+
+t_insert :: Ord a => a -> v -> RBT a v -> RBT a v
+t_insert x val s =
+    N B a v z b
+    where
+    N _ a v z b = ins s
+    ins E = N R E x val E
+    ins s@(N B a y val' b)
+        | x<y = t_balance (ins a) y val' b
+        | x>y = t_balance a y val' (ins b)
+        | otherwise = s
+    ins s@(N R a y val' b)
+        | x<y = N R (ins a) y val' b
+        | x>y = N R a y val' (ins b)
+        | otherwise = s
+
+t_balance :: RBT a v -> a -> v -> RBT a v -> RBT a v
+t_balance (N R a x xv b) y yv (N R c z zv d) = N R (N B a x xv b) y yv (N B c z zv d)
+t_balance (N R (N R a x xv b) y yv c) z zv d = N R (N B a x xv b) y yv (N B c z zv d)
+t_balance (N R a x xv (N R b y yv c)) z zv d = N R (N B a x xv b) y yv (N B c z zv d)
+t_balance a x xv (N R b y yv (N R c z zv d)) = N R (N B a x xv b) y yv (N B c z zv d)
+t_balance a x xv (N R (N R b y yv c) z zv d) = N R (N B a x xv b) y yv (N B c z zv d)
+t_balance a x xv b = N B a x xv b
+
+t_balleft :: RBT a v -> a -> v -> RBT a v -> RBT a v
+t_balleft (N R a x xv b) y yv c = N R (N B a x xv b) y yv c
+t_balleft bl x xv (N B a y yv b) = t_balance bl x xv (N R a y yv b)
+t_balleft bl x xv (N R (N B a y yv b) z zv c) = N R (N B bl x xv a) y yv (t_balance b z zv (t_sub1 c))
+
+t_balright :: RBT a v -> a -> v -> RBT a v -> RBT a v
+t_balright a x xv (N R b y yv c) = N R a x xv (N B b y yv c)
+t_balright (N B a x xv b) y yv bl = t_balance (N R a x xv b) y yv bl
+t_balright (N R a x xv (N B b y yv c)) z zv bl = N R (t_balance (t_sub1 a) x xv b) y yv (N B c z zv bl)
+
+t_sub1 :: RBT a v -> RBT a v
+t_sub1 (N B a x xv b) = N R a x xv b
+t_sub1 _ = error "invariance violation"
+
+t_app :: RBT a v -> RBT a v -> RBT a v
+t_app E x = x
+t_app x E = x
+t_app (N R a x xv b) (N R c y yv d) =
+ case t_app b c of
+     N R b' z zv c' -> N R (N R a x xv b') z zv (N R c' y yv d)
+     bc -> N R a x xv (N R bc y yv d)
+t_app (N B a x xv b) (N B c y yv d) = 
+ case t_app b c of
+     N R b' z zv c' -> N R (N B a x xv b') z zv (N B c' y yv d)
+     bc -> t_balleft a x xv (N B bc y yv d)
+t_app a (N R b x xv c) = N R (t_app a b) x xv c
+t_app (N R a x xv b) c = N R a x xv (t_app b c)
+
 -- The original term-level code, taken from:
 -- https://www.cs.kent.ac.uk/people/staff/smk/redblack/rb.html
 --
@@ -1565,18 +1643,18 @@ winnowI = fmap unI . winnow @k @v @t
 -- {- Insertion and membership test as by Okasaki -}
 -- insert :: Ord a => a -> RB a -> RB a
 -- insert x s =
--- 	T B a z b
--- 	where
--- 	T _ a z b = ins s
--- 	ins E = T R E x E
--- 	ins s@(T B a y b)
--- 		| x<y = balance (ins a) y b
--- 		| x>y = balance a y (ins b)
--- 		| otherwise = s
--- 	ins s@(T R a y b)
--- 		| x<y = T R (ins a) y b
--- 		| x>y = T R a y (ins b)
--- 		| otherwise = s
+--  T B a z b
+--  where
+--  T _ a z b = ins s
+--  ins E = T R E x E
+--  ins s@(T B a y b)
+--      | x<y = balance (ins a) y b
+--      | x>y = balance a y (ins b)
+--      | otherwise = s
+--  ins s@(T R a y b)
+--      | x<y = T R (ins a) y b
+--      | x>y = T R a y (ins b)
+--      | otherwise = s
 -- 
 -- 
 -- {- balance: first equation is new,
@@ -1592,25 +1670,25 @@ winnowI = fmap unI . winnow @k @v @t
 -- member :: Ord a => a -> RB a -> Bool
 -- member x E = False
 -- member x (T _ a y b)
--- 	| x<y = member x a
--- 	| x>y = member x b
--- 	| otherwise = True
+--  | x<y = member x a
+--  | x>y = member x b
+--  | otherwise = True
 -- 
 -- {- deletion a la SMK -}
 -- delete :: Ord a => a -> RB a -> RB a
 -- delete x t =
--- 	case del t of {T _ a y b -> T B a y b; _ -> E}
--- 	where
--- 	del E = E
--- 	del (T _ a y b)
--- 	    | x<y = delformLeft a y b
--- 	    | x>y = delformRight a y b
+--  case del t of {T _ a y b -> T B a y b; _ -> E}
+--  where
+--  del E = E
+--  del (T _ a y b)
+--      | x<y = delformLeft a y b
+--      | x>y = delformRight a y b
 --             | otherwise = app a b
--- 	delformLeft a@(T B _ _ _) y b = balleft (del a) y b
--- 	delformLeft a y b = T R (del a) y b
+--  delformLeft a@(T B _ _ _) y b = balleft (del a) y b
+--  delformLeft a y b = T R (del a) y b
 --
--- 	delformRight a y b@(T B _ _ _) = balright a y (del b)
--- 	delformRight a y b = T R a y (del b)
+--  delformRight a y b@(T B _ _ _) = balright a y (del b)
+--  delformRight a y b = T R a y (del b)
 -- 
 -- balleft :: RB a -> a -> RB a -> RB a
 -- balleft (T R a x b) y c = T R (T B a x b) y c
@@ -1630,13 +1708,13 @@ winnowI = fmap unI . winnow @k @v @t
 -- app E x = x
 -- app x E = x
 -- app (T R a x b) (T R c y d) =
--- 	case app b c of
--- 	    T R b' z c' -> T R(T R a x b') z (T R c' y d)
--- 	    bc -> T R a x (T R bc y d)
+--  case app b c of
+--      T R b' z c' -> T R(T R a x b') z (T R c' y d)
+--      bc -> T R a x (T R bc y d)
 -- app (T B a x b) (T B c y d) = 
--- 	case app b c of
--- 	    T R b' z c' -> T R(T B a x b') z (T B c' y d)
--- 	    bc -> balleft a x (T B bc y d)
+--  case app b c of
+--      T R b' z c' -> T R(T B a x b') z (T B c' y d)
+--      bc -> balleft a x (T B bc y d)
 -- app a (T R b x c) = T R (app a b) x c
 -- app (T R a x b) c = T R a x (app b c)
 
