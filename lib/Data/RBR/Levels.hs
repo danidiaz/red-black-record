71512c9fc8727eb61ffffa74722fcb62f64afc98
Daniel Diaz <diaz.carrete@facebook.com>
2019-09-01 23:27:50 +0200

    problems when pattern-matching

--- lib/Data/RBR/Levels.hs
+++ lib/Data/RBR/Levels.hs
@@ -27,6 +27,7 @@ import           Data.Proxy
 import           Data.Kind
 import           GHC.TypeLits
 
+import           Data.SOP (I(..))
 import           Data.RBR.Internal
 
 data Levels s q = Product (Map s (Levels s q))
@@ -34,10 +35,27 @@ data Levels s q = Product (Map s (Levels s q))
                 | Leaf q
                 deriving (Show,Eq)
 
--- class Stuff  (start :: Map Symbol Type)
---              (result :: Map Symbol (Levels Symbol Type)) | start -> result, result -> start where
+class Peel (start :: Map Symbol Type)
+           (result :: Map Symbol (Levels Symbol Type)) | start -> result, result -> start where
+
+instance Peel E E
+
+instance (Peel left' left, Peel right' right) =>
+         Peel (N color left' k (Multilevel sublevel) right')
+              (N color left  k sublevel              right )
 
 data Multilevel (levels :: Levels Symbol Type)  where
     Atom :: v -> Multilevel (Leaf v)
-    -- Record :: Record I t -> 
-    --Record I t -> Multilevel (Product (Map k levelsbelow))
+    Record :: Peel t' t => Record I t' -> Multilevel (Product t)
+    Variant :: Peel t' t => Variant I t' -> Multilevel (Sum t)
+
+
+foo :: Multilevel (Product (FromList '[ '("foo", Leaf Char) ]))
+foo = Record $ insertI @"foo" (Atom 'a') 
+             $ unit
+
+-- This doesn't work. 
+-- foodo :: Char
+-- foodo = case foo of
+--     Record thefoo -> case getFieldI @"foo" thefoo of
+--         Atom c -> c
