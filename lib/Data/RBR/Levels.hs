243b17a41571c63748747c633b8410513e9b15e1
Daniel Diaz <diaz.carrete@facebook.com>
2019-09-05 23:02:47 +0200

    compiles

--- lib/Data/RBR/Levels.hs
+++ lib/Data/RBR/Levels.hs
@@ -30,31 +30,31 @@ import           GHC.TypeLits
 import           Data.SOP (I(..))
 import           Data.RBR.Internal
 
-data Levels s q = Product (Map s (Levels s q))
-                | Sum (Map s (Levels s q))
-                | Leaf q
+data Levels s q = Product_ (Map s (Levels s q))
+                | Sum_ (Map s (Levels s q))
+                | Leaf_ q
                 deriving (Show,Eq)
 
 newtype Y (levelz :: Levels Symbol Type) = Y (Multilevel levelz) 
 
 data Multilevel (levels :: Levels Symbol Type)  where
-    Atom :: v -> Multilevel (Leaf v)
-    Record :: Record Y t -> Multilevel (Product t)
-    Variant :: Variant Y t -> Multilevel (Sum t)
+    Atom :: v -> Multilevel (Leaf_ v)
+    Record :: Record Y t -> Multilevel (Product_ t)
+    Variant :: Variant Y t -> Multilevel (Sum_ t)
 
 type (::>) a b = '(a, b)
-type (::.) a b = '(a, Atom b)
+type (::.) a b = '(a, Leaf_ b)
 
 type family ProductOf (input :: [(Symbol,Levels Symbol q)]) :: Levels Symbol q where
-    ProductOf pairs = Product (FromList pairs)
+    ProductOf pairs = Product_ (FromList pairs)
 
 type family SumOf (input :: [(Symbol,Levels Symbol q)]) :: Levels Symbol q where
-    SumOf pairs = Sum (FromList pairs)
+    SumOf pairs = Sum_ (FromList pairs)
 
-foo :: Multilevel (Product (FromList '[ '("foo", Leaf Char),
-                                        '("bar", Sum (FromList '[ '("sub1", Leaf Char),
-                                                                  '("sub2", Sum (FromList '[ '("subsub1", Leaf Int), 
-                                                                                             '("subsub2", Leaf Char) ]))]))]))
+foo :: Multilevel (Product_ (FromList '[ '("foo", Leaf_ Char),
+                                        '("bar", Sum_ (FromList '[ '("sub1", Leaf_ Char),
+                                                                  '("sub2", Sum_ (FromList '[ '("subsub1", Leaf_ Int), 
+                                                                                             '("subsub2", Leaf_ Char) ]))]))]))
 foo = Record $ insert @"foo" (Y (Atom 'a'))
              . insert @"bar" (Y (Variant $ inject @"sub1" (Y (Atom 'a'))))
              $ unit
@@ -67,7 +67,7 @@ bar :: Multilevel (ProductOf [
                                 "sub1" ::. Char,
                                 "sub2" ::>
                                     SumOf [
-                                        "subsub1" ::. Int
+                                        "subsub1" ::. Int,
                                         "subsub2" ::. Char
                                     ]
                             ]
